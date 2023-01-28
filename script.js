1f65aefde4697e919da523d456e45b85d544b74d
Chris Lupton <42554017+clupt@users.noreply.github.com>
2023-01-28 13:12:57 -0600

    add resume and footer styles

--- script.js
+++ script.js
@@ -1,3 +1,20 @@
+//make navbar sticky to the top
+const navbar = document.getElementById('top-navbar');
+let sticky = navbar.offsetTop;
+
+window.onscroll = function () {
+  stickyTop();
+};
+
+function stickyTop() {
+  if (window.pageYOffset >= sticky) {
+    navbar.classList.add('sticky');
+  } else {
+    navbar.classList.remove('sticky');
+  }
+}
+
+//toggle bio when click on photo of face
 const faceBtn = document.querySelector('#face-button');
 
 function toggleBio() {
